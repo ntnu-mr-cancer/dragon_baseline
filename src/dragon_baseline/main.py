3c6ad7ef39027bf5d1e2e5ecda9237146b04424d
Joeran Bosma <joeran@bosma.co>
2024-09-13 17:41:39 +0200

    Cleanup (#1)
    * Update development dependencies
    * Cleanup

--- src/dragon_baseline/main.py
+++ src/dragon_baseline/main.py
@@ -661,7 +661,15 @@ class DragonBaseline(NLPAlgorithm):
 if __name__ == "__main__":
     # Note: to debug (outside of Docker), you can set the input and output paths.
     for job_name in [
-        "Task202_pirads_lesion_location_ner-fold0"
+        "Task101_Example_sl_bin_clf-fold0",
+        "Task102_Example_sl_mc_clf-fold0",
+        "Task103_Example_mednli-fold0",
+        "Task104_Example_ml_bin_clf-fold0",
+        "Task105_Example_ml_mc_clf-fold0",
+        "Task106_Example_sl_reg-fold0",
+        "Task107_Example_ml_reg-fold0",
+        "Task108_Example_sl_ner-fold0",
+        "Task109_Example_ml_ner-fold0",
     ]:
         DragonBaseline(
             input_path=Path(f"test-input/{job_name}"),

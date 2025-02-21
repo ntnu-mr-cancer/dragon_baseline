802240aa23c794bb8aacc2672a84cf4ae823e166
Joeran Bosma <joeran@bosma.co>
2025-02-21 16:18:07 +0100

    Improve test method

--- test.sh
+++ test.sh
@@ -6,19 +6,19 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 VOLUME_SUFFIX=$(dd if=/dev/urandom bs=32 count=1 | md5sum | cut -c 1-10)
 # Maximum is currently 30g, configurable in your algorithm image settings on grand challenge
-MEM_LIMIT="4g"
+MEM_LIMIT="15g"
 
-test_result="Test result: "
 
-for fold in 0
-do
+# set up temporary volumes for the docker run
+docker volume create dragon-output-$VOLUME_SUFFIX
+
+fold=0
 for task_name in "Task101_Example_sl_bin_clf" "Task102_Example_sl_mc_clf" "Task103_Example_mednli" "Task104_Example_ml_bin_clf" "Task105_Example_ml_mc_clf" "Task106_Example_sl_reg" "Task107_Example_ml_reg" "Task108_Example_sl_ner" "Task109_Example_ml_ner"
 do
     jobname="$task_name-fold$fold"
 
     echo "=========================================="
     echo "Running test for $jobname..."
-    docker volume create dragon_baseline-output-$VOLUME_SUFFIX
 
     # Do not change any of the parameters to docker run, these are fixed
     docker run --rm \
@@ -31,33 +31,34 @@ do
         --shm-size="128m" \
         --pids-limit="256" \
         -v $SCRIPTPATH/test-input/$jobname:/input:ro \
-        -v dragon_baseline-output-$VOLUME_SUFFIX:/output/ \
-        joeranbosma/dragon_baseline
+        -v dragon-output-$VOLUME_SUFFIX:/output \
+        joeranbosma/dragon_baseline:latest
 
+    # Display the output file
     docker run --rm \
-        -v dragon_baseline-output-$VOLUME_SUFFIX:/output/ \
+        -v dragon-output-$VOLUME_SUFFIX:/output/ \
         python:3.10-slim cat /output/nlp-predictions-dataset.json
 
-    echo ""
-
+    # Collect the output file
     docker run --rm \
-        -v dragon_baseline-output-$VOLUME_SUFFIX:/output/ \
-        -v $SCRIPTPATH/test-output-expected/$jobname/:/output-expected/ \
-        python:3.10-slim python -c "import json, sys; f1 = json.load(open('/output/nlp-predictions-dataset.json')); f2 = json.load(open('/output-expected/nlp-predictions-dataset.json')); sys.exit(f1 != f2);"
+        -v "dragon-output-$VOLUME_SUFFIX":/output \
+        alpine /bin/sh -c "mkdir -p /output/$jobname; mv /output/nlp-predictions-dataset.json /output/$jobname/;"
 
-    if [ $? -eq 0 ]; then
-        echo "Test for $jobname successfully passed..."
-        test_result="$test_result $jobname:pass"
-    else
-        echo "Expected output was not found for $jobname..."
-        test_result="$test_result $jobname:fail"
-    fi
-
-    docker volume rm dragon_baseline-output-$VOLUME_SUFFIX
-
-done
 done
 
 echo "=========================================="
-echo "$test_result"
+echo "Test result: "
+
+# Evaluate the output
+docker run --rm \
+    -v dragon-output-$VOLUME_SUFFIX:/input:ro \
+    -v $SCRIPTPATH/test-ground-truth:/opt/app/ground-truth \
+    --entrypoint python \
+    joeranbosma/dragon_baseline:latest -m dragon_eval --folds 0
+
+docker volume rm dragon-output-$VOLUME_SUFFIX
+
+echo "Please check that all performances are above random guessing! For tasks 101-107, the performance should be above 0.7, for tasks 108-109 above 0.2."
+echo "=========================================="
+echo "Test completed."
 echo "=========================================="
